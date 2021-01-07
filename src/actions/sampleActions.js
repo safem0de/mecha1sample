c5c9665ac1fbb61525307b1969dff7ec033cd84b
Naweeparb Wangwanich <n.wangwanich@mail.com>
2021-01-07 17:10:58 +0700

    countSamples

--- src/actions/sampleActions.js
+++ src/actions/sampleActions.js
@@ -41,6 +41,32 @@ export function getSamples(){
 }
 
 export function countSamples(){
+    const genDataset = (label,rgb) =>{
+        const color = 'rgba('+ rgb[0]+','+ rgb[1]+','+ rgb[2]+',0.4)'
+        const colorBorder = 'rgba('+ rgb[0]+','+ rgb[1]+','+ rgb[2]+',1)'
+        const struct = {
+            label: label,
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: color,
+            borderColor: colorBorder,
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: colorBorder,
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: colorBorder,
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+        return struct;
+    }
 }
 
 export function saveSample(sample,lt){
